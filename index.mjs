351bb884cd34d51f915ea85c365043d537ced1ae
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-03 14:36:26 +0100

    Update and rename index.min.ejs to index.mjs

--- index.mjs
+++ index.mjs
@@ -1,13 +1,3 @@
-
-/**
- * @license React
- * react-dom.development.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('react')) :
   typeof define === 'function' && define.amd ? define(['exports', 'react'], factory) :
